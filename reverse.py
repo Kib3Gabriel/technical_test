5b22a70b0738d1e9523e4f791fdb3a4600b95009
gabriel <gkibe12@gmail.com>
2024-06-21 15:42:14 +0300

    Changed Reversed to Number reversed

--- reverse.py
+++ reverse.py
@@ -1,20 +1,8 @@
-
-# def reverse(n):
-#     rev = 0
-
-#     while(n>0):
-#         a = n%10
-#         rev= rev *10 +a
-#         n =n // 10
-#     return rev
-
-        
-
+#Design a function that reverses the digits of an integer. For example, 50 should become 5 and -12 should become -21.
 def reverse(number):
     rev = 0
-    # Check if the number is negative
+    
     negative_num = number < 0
-    # Work with the absolute value of the number
     number = abs(number)
     
     while number > 0:
@@ -22,11 +10,8 @@ def reverse(number):
         rev = rev * 10 + a
         number = number // 10
     
-    # If the number was negative, return the negative of the reversed number
     return -rev if negative_num else rev
 
-# Prompt the user to enter a number
-# print("Enter a number: ")
 number = int(input("Enter a number: "))
 
-print('Reversed:',reverse(number))
+print('Number reversed:',reverse(number))
