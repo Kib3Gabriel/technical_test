ac5ec11d54d11c625eebf875592b54b2e975a4d3
gabriel <gkibe12@gmail.com>
2024-06-21 16:56:36 +0300

    removed a full stop

--- pangram.py
+++ pangram.py
@@ -2,7 +2,7 @@
 #A pangram is a sentence or phrase containing every letter of the alphabet at
 #least once. Punctuation and case are typically ignored. For example, the
 #string "The quick brown fox jumps over the lazy dog" is a pangram, while
-#"Hello, world!" is not.
+#"Hello, world!" is not
 
 def isPangram(string):
     alphabet_char = "abcdefghklmnopqrstuvwz"
